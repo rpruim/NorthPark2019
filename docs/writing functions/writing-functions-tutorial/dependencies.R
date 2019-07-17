5abd145d00df80004eab943384dc6bb623f46c97
Randall Pruim <rpruim@gmail.com>
2019-07-17 08:20:55 -0500

    add stuff for wednesday

---
+++ docs/writing functions/writing-functions-tutorial/dependencies.R
@@ -0,0 +1,9 @@
+library(learnr)     # the tutorial package
+library(tibble)     # for dealing with tibble data
+library(ggformula)  # all those gf_functions live hear
+library(mosaicData) # mosaic data sets
+library(mosaic)     # Project MOSAIC tools 
+require(googlesheets)
+require(dplyr)
+require(readr)
+require(tidyr)
