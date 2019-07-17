d2d2d253354326cbe4dc022bdb51dc6e5b2262e4
Randall Pruim <rpruim@gmail.com>
2019-07-17 15:03:39 -0500

    delete old named folder

--- docs/writing functions/writing-functions-tutorial/dependencies.R
+++
@@ -1,9 +0,0 @@
-library(learnr)     # the tutorial package
-library(tibble)     # for dealing with tibble data
-library(ggformula)  # all those gf_functions live hear
-library(mosaicData) # mosaic data sets
-library(mosaic)     # Project MOSAIC tools 
-require(googlesheets)
-require(dplyr)
-require(readr)
-require(tidyr)
